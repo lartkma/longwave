69e1796668ab3b6ae50a5dab1e37ec608e590156
Luis Ramirez <lartkma@gmail.com>
2020-06-30 12:25:04 -0500

    Store locale search results

--- src/locale/GetLocaleData.ts
+++ src/locale/GetLocaleData.ts
@@ -4,6 +4,8 @@ import esLocaleData from "./es/LocaleData";
 
 const allLocales = [enLocaleData, esLocaleData];
 
+let localeMap: {[key: string]: LocaleData} = {};
+
 export const localeList = [
   {
     prefix: 'en',
@@ -15,5 +17,10 @@ export const localeList = [
 ];
 
 export function GetLocaleData(localeCode = 'en'): LocaleData {
-  return allLocales.find(x => x.prefix === localeCode) || allLocales[0];
+  let locale = localeMap[localeCode];
+  if (!locale) {
+    locale = allLocales.find(x => x.prefix === localeCode) || allLocales[0];
+    localeMap[localeCode] = locale;
+  }
+  return locale;
 }
